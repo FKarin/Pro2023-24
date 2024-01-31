7852de03fc096cdf956a37cdb4e5db752742c278
Karin Fornazarič <fornazarickarin@gmail.com>
2024-01-31 10:57:35 +0100

    Razredi vaje

---
+++ RazrediVaje/RazrediVaje/Krog.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazrediVaje
+{
+    internal class Krog
+    {
+        private int x, y;
+        private float r;
+
+        public int X { get => x; set => x= value; }
+        public int Y { get => y; set => y = value; }
+        public float Radij { get => r; set => r = value; }
+
+        public Krog(int x, int y, float r)
+        {
+            int xK = this.x;
+            int yK = this.y;
+            float rK = this.r;
+        }
+
+        public double Premer()
+        {
+            return r * 2;
+        }
+        public double Ploščina()
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
---
+++ RazrediVaje/RazrediVaje/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazrediVaje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Krog k = new Krog(37, 45, 2.5f);
+            Console.WriteLine("X koordinata = " + k.X +
+            "\nY koordinata = " + k.Y +
+            "\nRadij =" + k.Radij);
+            Console.WriteLine("Premer = " + k.Premer());
+            Console.WriteLine("Plošcina =" + k.Ploščina());
+            k.X = 2;
+            k.Y = 2;
+            k.Radij = 4.25f;
+            Console.WriteLine("Nova pozicija :");
+            Console.WriteLine("X koordinata = " + k.X +
+            "\nY koordinata = " + k.Y +
+            "\nRadij =" + k.Radij);
+            Console.WriteLine("Premer = " + k.Premer());
+            Console.WriteLine("Plošcina =" + k.Ploščina());
+            Console.ReadLine();
+
+            Pravokotnik p = new Pravokotnik(0, 1, 1, 0);
+            Console.WriteLine("Koordinati kota levo zgoraj = " + p.X1 + "," + p.Y1 +
+            "\nKoordinati kota desno spodaj = " + p.X2 + "," + p.Y2);
+            Console.WriteLine("Stranica a " + p.StranicaA());
+            Console.WriteLine("Stranica b " + p.StranicaB());
+            Console.WriteLine("Plošina " + p.Plošcina());
+        }
+    }
+}
