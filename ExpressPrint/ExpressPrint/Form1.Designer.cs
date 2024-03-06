63334079da138ca1f6160f1d4504478a7c0c9a0a
Karin Fornazarič <fornazarickarin@gmail.com>
2024-03-06 10:56:13 +0100

    Photo Editor

---
+++ ExpressPrint/ExpressPrint/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ExpressPrint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Datoteka = new System.Windows.Forms.ToolStripMenuItem();
+            this.odpriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.izhodToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shraniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vrtenjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.za90ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.za180ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.za270ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zrcaljenjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.navpičnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vodoravnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filitriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.posvetliToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.potemniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.negativToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dlgOdpri = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.vScrollBar1.Location = new System.Drawing.Point(783, 24);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 426);
+            this.vScrollBar1.TabIndex = 1;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hScrollBar1.Location = new System.Drawing.Point(0, 433);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(783, 17);
+            this.hScrollBar1.TabIndex = 2;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Datoteka,
+            this.vrtenjeToolStripMenuItem,
+            this.zrcaljenjeToolStripMenuItem,
+            this.filitriToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Datoteka
+            // 
+            this.Datoteka.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.odpriToolStripMenuItem,
+            this.izhodToolStripMenuItem,
+            this.shraniToolStripMenuItem});
+            this.Datoteka.Name = "Datoteka";
+            this.Datoteka.Size = new System.Drawing.Size(66, 20);
+            this.Datoteka.Text = "Datoteka";
+            // 
+            // odpriToolStripMenuItem
+            // 
+            this.odpriToolStripMenuItem.Name = "odpriToolStripMenuItem";
+            this.odpriToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.odpriToolStripMenuItem.Text = "Odpri";
+            this.odpriToolStripMenuItem.Click += new System.EventHandler(this.odpriToolStripMenuItem_Click);
+            // 
+            // izhodToolStripMenuItem
+            // 
+            this.izhodToolStripMenuItem.Name = "izhodToolStripMenuItem";
+            this.izhodToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.izhodToolStripMenuItem.Text = "Shrani";
+            // 
+            // shraniToolStripMenuItem
+            // 
+            this.shraniToolStripMenuItem.Name = "shraniToolStripMenuItem";
+            this.shraniToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.shraniToolStripMenuItem.Text = "Izhod";
+            // 
+            // vrtenjeToolStripMenuItem
+            // 
+            this.vrtenjeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.za90ToolStripMenuItem,
+            this.za180ToolStripMenuItem,
+            this.za270ToolStripMenuItem});
+            this.vrtenjeToolStripMenuItem.Name = "vrtenjeToolStripMenuItem";
+            this.vrtenjeToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.vrtenjeToolStripMenuItem.Text = "Vrtenje";
+            // 
+            // za90ToolStripMenuItem
+            // 
+            this.za90ToolStripMenuItem.Name = "za90ToolStripMenuItem";
+            this.za90ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.za90ToolStripMenuItem.Text = "Za 90°";
+            this.za90ToolStripMenuItem.Click += new System.EventHandler(this.za90ToolStripMenuItem_Click);
+            // 
+            // za180ToolStripMenuItem
+            // 
+            this.za180ToolStripMenuItem.Name = "za180ToolStripMenuItem";
+            this.za180ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.za180ToolStripMenuItem.Text = "Za 180°";
+            this.za180ToolStripMenuItem.Click += new System.EventHandler(this.za180ToolStripMenuItem_Click);
+            // 
+            // za270ToolStripMenuItem
+            // 
+            this.za270ToolStripMenuItem.Name = "za270ToolStripMenuItem";
+            this.za270ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.za270ToolStripMenuItem.Text = "Za 270°";
+            this.za270ToolStripMenuItem.Click += new System.EventHandler(this.za270ToolStripMenuItem_Click);
+            // 
+            // zrcaljenjeToolStripMenuItem
+            // 
+            this.zrcaljenjeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.navpičnoToolStripMenuItem,
+            this.vodoravnoToolStripMenuItem});
+            this.zrcaljenjeToolStripMenuItem.Name = "zrcaljenjeToolStripMenuItem";
+            this.zrcaljenjeToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.zrcaljenjeToolStripMenuItem.Text = "Zrcaljenje";
+            // 
+            // navpičnoToolStripMenuItem
+            // 
+            this.navpičnoToolStripMenuItem.Name = "navpičnoToolStripMenuItem";
+            this.navpičnoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.navpičnoToolStripMenuItem.Text = "Navpično";
+            this.navpičnoToolStripMenuItem.Click += new System.EventHandler(this.navpičnoToolStripMenuItem_Click);
+            // 
+            // vodoravnoToolStripMenuItem
+            // 
+            this.vodoravnoToolStripMenuItem.Name = "vodoravnoToolStripMenuItem";
+            this.vodoravnoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.vodoravnoToolStripMenuItem.Text = "Vodoravno";
+            this.vodoravnoToolStripMenuItem.Click += new System.EventHandler(this.vodoravnoToolStripMenuItem_Click);
+            // 
+            // filitriToolStripMenuItem
+            // 
+            this.filitriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.posvetliToolStripMenuItem,
+            this.potemniToolStripMenuItem,
+            this.negativToolStripMenuItem});
+            this.filitriToolStripMenuItem.Name = "filitriToolStripMenuItem";
+            this.filitriToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.filitriToolStripMenuItem.Text = "Filitri";
+            // 
+            // posvetliToolStripMenuItem
+            // 
+            this.posvetliToolStripMenuItem.Name = "posvetliToolStripMenuItem";
+            this.posvetliToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.posvetliToolStripMenuItem.Text = "Posvetli";
+            this.posvetliToolStripMenuItem.Click += new System.EventHandler(this.posvetliToolStripMenuItem_Click);
+            // 
+            // potemniToolStripMenuItem
+            // 
+            this.potemniToolStripMenuItem.Name = "potemniToolStripMenuItem";
+            this.potemniToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.potemniToolStripMenuItem.Text = "Potemni";
+            this.potemniToolStripMenuItem.Click += new System.EventHandler(this.potemniToolStripMenuItem_Click);
+            // 
+            // negativToolStripMenuItem
+            // 
+            this.negativToolStripMenuItem.Name = "negativToolStripMenuItem";
+            this.negativToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.negativToolStripMenuItem.Text = "Negativ";
+            this.negativToolStripMenuItem.Click += new System.EventHandler(this.negativToolStripMenuItem_Click);
+            // 
+            // dlgOdpri
+            // 
+            this.dlgOdpri.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Datoteka;
+        private System.Windows.Forms.ToolStripMenuItem odpriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem izhodToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shraniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vrtenjeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem za90ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem za180ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem za270ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zrcaljenjeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem navpičnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vodoravnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filitriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem posvetliToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem potemniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem negativToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog dlgOdpri;
+    }
+}
+
---
+++ ExpressPrint/ExpressPrint/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpressPrint
+{
+    public partial class Form1 : Form
+    {
+        Bitmap original = new Bitmap(640, 400);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void izhodToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void odpriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dlgOdpri.Filter = "Slike(*.jpg, *.png, *.bmp)|*.jpg;*.png;*.bmp";
+            DialogResult a = dlgOdpri.ShowDialog();
+            if (a != DialogResult.OK)
+                return;
+            string imeDatoteke = dlgOdpri.FileName;
+            original = new Bitmap(imeDatoteke);
+            pictureBox1.Image = original;
+            PosodobiSliko();
+        }
+
+        private void NastaviDrsnike()
+        {
+            if(original.Width < pictureBox1.Width)
+                hScrollBar1.Visible = false;
+            else
+            {
+                hScrollBar1.Visible = true;
+                hScrollBar1.Maximum = original.Width - pictureBox1.Width;
+            }
+            if (original.Height < pictureBox1.Height)
+                vScrollBar1.Visible = false;
+            else
+            {
+                vScrollBar1.Visible = true;
+                vScrollBar1.Maximum = original.Height - pictureBox1.Height;
+            }
+        }
+
+        private void PosodobiSliko()
+        {
+            NastaviDrsnike();
+            Graphics g = pictureBox1.CreateGraphics();
+            Rectangle r1 = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+            Rectangle r2 = new Rectangle(hScrollBar1.Value, vScrollBar1.Value, pictureBox1.Width, pictureBox1.Height);
+            g.DrawImage(original, r1, r2, GraphicsUnit.Pixel);
+            pictureBox1.Update();
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            PosodobiSliko();
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            PosodobiSliko();
+        }
+
+        private void navpičnoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Color pixel;
+            for(int x = 0; x < original.Width / 2; x++)
+            {
+                for(int y = 0; y < original.Height; y++)
+                {
+                    pixel = original.GetPixel(x, y);
+                    original.SetPixel(original.Width - x - 1, y, pixel);
+                }
+                PosodobiSliko();
+            }
+        }
+
+        private void negativToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < original.Width / 2; x++)
+            {
+                for (int y = 0; y < original.Height; y++)
+                {
+                    Color barva = original.GetPixel(x, y);
+                    int r = 255 - barva.R;
+                    int g = 255 - barva.G;
+                    int b = 255 - barva.B;
+                    barva = Color.FromArgb(r, g, b);
+                    original.SetPixel(x, y, barva);
+                }
+                PosodobiSliko();
+            }
+        }
+
+        private void za90ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            original.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            PosodobiSliko();
+        }
+
+        private void za180ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            original.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            PosodobiSliko();
+        }
+
+        private void za270ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            original.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            PosodobiSliko();
+        }
+
+        private void vodoravnoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Color pixel;
+            for (int x = 0; x < original.Width; x++)
+            {
+                for (int y = 0; y < original.Height / 2; y++)
+                {
+                    pixel = original.GetPixel(x, y);
+                    original.SetPixel(x, original.Height - y - 1, pixel);
+                }
+                PosodobiSliko();
+            }
+        }
+
+        private void posvetliToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Color pixel;
+            int r, g, b;
+            for (int y = 0; y < original.Height; y++)
+            {
+                for (int x = 0; x < original.Width; x++) //po širini
+                {
+                    pixel = original.GetPixel(x, y); //barva pixla
+                    r = (int)((double)pixel.R * 0.9); //posvetli rdečo
+                    if (r < 0) r = 0;
+                    if (r > 255) r = 255;
+                    g = (int)((double)pixel.G * 0.9);//posvetli zelo
+                    if (g < 0) g = 0;
+                    if (g > 255) g = 255;
+                    b = (int)((double)pixel.B * 0.9);//posvetli modro
+                    if (b < 0) b = 0;
+                    if (b > 255) b = 255;
+                    original.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+                PosodobiSliko();
+            } // Po vsej višini slike
+        }
+
+        private void potemniToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Color pixel;
+            int r, g, b;
+            for (int y = 0; y < original.Height; y++)
+            {
+                for (int x = 0; x < original.Width; x++) //po širini
+                {
+                    pixel = original.GetPixel(x, y); //barva pixla
+                    r = (int)((double)pixel.R * 1.1); //posvetli rdečo
+                    if (r < 0) r = 0;
+                    if (r > 255) r = 255;
+                    g = (int)((double)pixel.G * 1.1);//posvetli zelo
+                    if (g < 0) g = 0;
+                    if (g > 255) g = 255;
+                    b = (int)((double)pixel.B * 1.1);//posvetli modro
+                    if (b < 0) b = 0;
+                    if (b > 255) b = 255;
+                    original.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+                PosodobiSliko();
+            } // Po vsej višini slike
+        }
+    }
+}
